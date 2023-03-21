8d40a4fc2d96f99fdba0b656b6604895bfedbba0
Matheus N. Leite <matheusn06leite@gmail.com>
2023-03-21 20:20:19 -0300

    desafio03 e desafio 04

---
+++ Aula6/desafio03.py
@@ -0,0 +1,11 @@
+#num1=input('Primeiro número ')
+#num2=input('Segundo número ')
+#soma=int(num1) + int(num2)
+#print('A soma é', soma)
+
+#OU
+
+num1=int(input('Primeiro número '))
+num2=int(input('Segundo número '))
+soma= num1 + num2
+print('A soma é', soma)
