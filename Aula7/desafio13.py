1463b53f21b6289d86b0f32dea831cf690287e8c
Matheus N. Leite <matheusn06leite@gmail.com>
2023-03-29 16:46:50 -0300

    correção desafio13.py: duas casas apos a virgula

--- Aula7/desafio13.py
+++ Aula7/desafio13.py
@@ -2,4 +2,4 @@ salario_atual=float(input('Salario atual: '))
 
 novo_salario = salario_atual + ((15/100) * salario_atual)
 
-print('Seu novo salario é: R${}'.format(novo_salario))
+print('Seu novo salario é: R${:.2f}'.format(novo_salario))
