31ab5073e98d774db7aba4649eeec6d20caaf13e
Matheus N. Leite <matheusn06leite@gmail.com>
2023-03-29 16:05:30 -0300

    Aprimoramento desafio08: adicionado medidas km, h, da, d

--- Aula7/desafio08.py
+++ Aula7/desafio08.py
@@ -1,5 +1,9 @@
 n=float(input('Digite um valor em metros: '))
+k = n*0.001
+h = n*0.01
+da = n*0.1
+d = n*10
 c = n*100
 mm = n*1000
 
-print('Metros: {}\nCentimetros: {}\nMilimetros: {}'.format(n, c, mm))
+print('Quilometros: {}\nHectometros: {}\nDecâmetros: {}\nMetros: {}\nDecimetros: {}\nCentimetros: {}\nMilimetros: {}'.format(k,h,da,n,d,c,mm))
