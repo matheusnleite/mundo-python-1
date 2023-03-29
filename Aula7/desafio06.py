f071495a62819a4f9ff6b6322b274742b8e0ae68
Matheus N. Leite <matheusn06leite@gmail.com>
2023-03-29 15:48:25 -0300

    correção no desafio06: correção na raiz quadrada (mostrar apenas duias casas após o ponto)

--- Aula7/desafio06.py
+++ Aula7/desafio06.py
@@ -1,2 +1,2 @@
 n=int(input('Digite um numero: '))
-print('Numero escolhido: {}\nDobro: {}\nTriplo: {}\nRaiz quadrada: {}'.format(n, n*2, n*3, n**(1/2)))
+print('Numero escolhido: {}\nDobro: {}\nTriplo: {}\nRaiz quadrada: {:.2f}'.format(n, n*2, n*3, n**(1/2)))
